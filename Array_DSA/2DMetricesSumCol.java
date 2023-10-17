760bfc7ead3cf2a8da3d0193e798d16c1b46eb72
Tushar Kokane <tusharkokane982@gmail.com>
2023-10-18 00:45:58 +0530

    2DMetrices

---
+++ Array_DSA/2DMetricesSumCol.java
@@ -0,0 +1,17 @@
+//Sum of ele col by col
+class Sum2DCol{
+     
+    public static void main(String[]args){
+        int arr[][]=new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        for(int i=0;i<arr[0].length;i++){
+            int sum=0;
+            for(int j=0;j<arr.length;j++){
+                sum=sum+arr[j][i];
+                
+            }
+            System.out.println(sum);
+        }
+
+        
+    }
+}
